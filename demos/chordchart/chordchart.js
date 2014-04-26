a6fd28ca6218f1a74f521695271939a2bd9fd9bc
Tanner Langley <tlangley141@gmail.com>
2014-04-26 01:22:44 -0600

    Created a way to split lines in lyrics to allow for chords above each line

--- demos/chordchart/chordchart.js
+++ demos/chordchart/chordchart.js
@@ -1,7 +1,25 @@
 $(function () {
-	
-	var lyric = $.ajax({
-		url: 'http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=thrice',
+	var lyrics = $(document).find("#lyrics");
+
+	var spanInserted = lyrics.html().split(" ").join(" </span><span>");
+	var wrapped = ("<span>").concat(spanInserted, "</span>");
+	lyrics.html(wrapped);
+	var refPos = lyrics.find('span:first-child').position().top;
+	var newPos;
+	lyrics.find('span').each(function(index) {
+	    newPos = $(this).position().top   
+	    if (index == 0){
+	       return;
+	    }
+	    if (newPos == refPos){
+	        $(this).prepend($(this).prev().text() + " ");
+	        $(this).prev().remove();
+	    } 
+	    refPos = newPos;
 	});
-	console.log(lyric);
+
+	lyrics.find('span').click(function () {
+	    $(this).addClass('chords');
+	});
+
 });
---
+++ demos/chordchart/chordchart.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chord Chart</title>
+	<link rel="stylesheet" href="../../stylesheets/screen.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	<script src="chordchart.js"></script>
+	<style>
+		#lyrics span {
+			cursor: pointer;
+		}
+		#lyrics span.chords {
+			display: inline-block;
+			padding-top: 1.5em;					
+		}
+	</style>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<aside id="toolbar" class="col-md-3">
+				<button type="button" class="btn btn-default btn-sm">
+					<span class="glyphicon glyphicon-music"></span>
+				</button>
+			</aside>
+			<section id="lyrics" class="col-md-9">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sint officiis doloribus voluptatem enim! Accusantium beatae assumenda consequatur aut porro eaque eveniet quae cum ducimus aliquam. Similique architecto neque consequuntur!
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, repellendus eos iure quos porro optio reiciendis ipsam reprehenderit excepturi veritatis dolorum aliquam animi eum rem temporibus quo quasi ut magnam.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, fugit, labore odit tenetur nulla inventore porro explicabo ipsum esse natus est corporis in minima quisquam quaerat cumque impedit commodi quidem.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reprehenderit, voluptas ducimus non sunt impedit ipsam culpa voluptatibus consequatur aspernatur ipsa sint sit tempora soluta dolorum ex laudantium et blanditiis.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sint officiis doloribus voluptatem enim! Accusantium beatae assumenda consequatur aut porro eaque eveniet quae cum ducimus aliquam. Similique architecto neque consequuntur!
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, repellendus eos iure quos porro optio reiciendis ipsam reprehenderit excepturi veritatis dolorum aliquam animi eum rem temporibus quo quasi ut magnam.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, fugit, labore odit tenetur nulla inventore porro explicabo ipsum esse natus est corporis in minima quisquam quaerat cumque impedit commodi quidem.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reprehenderit, voluptas ducimus non sunt impedit ipsam culpa voluptatibus consequatur aspernatur ipsa sint sit tempora soluta dolorum ex laudantium et blanditiis.		
+			</section>
+		</div>
+	</div>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
